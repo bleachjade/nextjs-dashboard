f4d899db38266a8aad579a92ff1d84c8bd08ef8d
bleachjade <bleachjade@hotmail.com>
2025-06-24 14:34:25 +0700

    [fix] fix issue

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,8 @@
 import { lusitana } from "@/app/ui/fonts";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { fetchCardData } from "@/app/lib/data";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import CardWrapper from "@/app/ui/dashboard/cards";
+import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import { Suspense } from "react";
 import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 
